e40e3bdcc14b3ebf422053f587c899bb7b74a44a
Bountham <bountham@Bounthams-MacBook-Pro.local>
2018-08-05 20:30:02 -0700

    add sample and update function

--- Ars.py
+++ Ars.py
@@ -1,12 +1,12 @@
 # AI 2018
 
 # Importing the libraries
- import os
- import numpy as np
+import os
+import numpy as np
 
 # Setting the Hyper Parameters
  
- class Hp ():
+class Hp ():
      
      def __init__(self):
          self.nb_steps =  1000
@@ -22,7 +22,7 @@
   
          
          # Normalisting the states
- class Nomalizer():
+class Nomalizer():
      
      def __init__(self, np_inputs):
          self.n = np.zeros(np_inputs)
@@ -44,14 +44,27 @@
          
  # Building  the AI
  
- class Policy():
-     
-     def __init__(self, input_size, output_size):
+class Policy():
+    
+    def __init__(self, input_size, output_size):
          self.theta = np.zeros((output.size, input_size))
  
- 
- 
- 
+    def evaluate(self, input, delta = None, direction = None):
+         if direction is None:
+             return self.theta.dot(input)
+         elif direction == "positive":
+             return (self.theta + hp.noise*delta).dot(input)
+         else:
+             return(self.theta - hp.noise*delta).do(input)
+         
+    def sample_deltas(self):
+        return [np.random.randn(*self.theta.shape) for _ in range(hp.nb_direction)]
+    
+    def update(self,rollouts, sigma_r):
+        step = np.zeros()(self.theta.shape)
+        for r_pos, r_neg, d in rollouts:
+            step += (r_pos - r_neg) * d
+            self.theta += hp.learning_rate / (hp.nb_best_direction * sigma_r) * step
  
  
  
